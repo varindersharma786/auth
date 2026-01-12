6a89dc613f55cfd3f2eb2eea5fad5eb8021a85c6
Varinder Sharma <varindersharma786786@gmail.com>
2026-01-12 11:35:17 +0530

    feat: adding server actions

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,11 +1,8 @@
 "use client";
-import Image from "next/image";
 import { authClient } from "@/lib/auth-client";
 
 export default function Home() {
-
   const { data: session } = authClient.useSession();
-
 
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
