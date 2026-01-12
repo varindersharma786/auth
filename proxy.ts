209fc62692c0d74923eeac44580826e1fa5a6e1f
Varinder Sharma <varindersharma786786@gmail.com>
2026-01-12 09:46:32 +0530

    feat: add shdcn, them changer

--- proxy.ts
+++ proxy.ts
@@ -2,7 +2,7 @@ import { betterFetch } from "@better-fetch/fetch";
 import { NextResponse, type NextRequest } from "next/server";
 import type { Session } from "better-auth/types";
 
-export default async function middleware(request: NextRequest) {
+export default async function proxy(request: NextRequest) {
     const { data: session } = await betterFetch<Session>(
         "/api/auth/get-session",
         {
